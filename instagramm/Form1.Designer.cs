4931315516f2f818fefe9d9c6c71719a70ab18e1
taran507 <tarrarran273@gmail.com>
2020-10-24 19:39:31 +1000

    ывва

--- instagramm/Form1.Designer.cs
+++ instagramm/Form1.Designer.cs
@@ -45,7 +45,7 @@
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(261, 223);
+            this.radioButton1.Location = new System.Drawing.Point(417, 228);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(85, 17);
             this.radioButton1.TabIndex = 1;
