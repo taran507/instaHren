5febcd46f0f1930664071d243e8bca14ac60d4e9
Baggi Wolf <baggi125@gmail.com>
2020-10-24 19:41:56 +1000

    ф

--- instagramm/Form1.Designer.cs
+++ instagramm/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(372, 282);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(160, 95);
             this.button1.TabIndex = 0;
