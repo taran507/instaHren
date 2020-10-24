22b79a591fa4c81e36f3e363f519b26631dc17a3
taran507 <tarrarran273@gmail.com>
2020-10-24 19:38:13 +1000

    вы

--- instagramm/Form1.Designer.cs
+++ instagramm/Form1.Designer.cs
@@ -33,9 +33,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(261, 255);
+            this.button1.Location = new System.Drawing.Point(372, 282);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(271, 122);
+            this.button1.Size = new System.Drawing.Size(160, 95);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
